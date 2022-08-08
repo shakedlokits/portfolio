5fc102a9e13cccc41aeebe94944973b41becf21a
Shaked Lokits <shaked.lokits@gmail.com>
2022-08-08 14:31:23 +0300

    Add tailwind.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import {useLogger} from '../lib/logger';
-import {DataProps} from "../lib/data-fetcher";
+import { useLogger } from '../lib/logger';
+import { DataProps } from '../lib/data-fetcher';
 
 export { getServerSideProps } from '../lib/data-fetcher';
 
-const Home = ({projects, works, articles}: DataProps) => {
-    const logger = useLogger();
-    logger.info({projects, works, articles}, 'Loaded documents');
+const Home = ({ projects, works, articles }: DataProps) => {
+  const logger = useLogger();
+  logger.info({ projects, works, articles }, 'Loaded documents');
 
-    return (<h1>Hello Portfolio</h1>);
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default Home;
