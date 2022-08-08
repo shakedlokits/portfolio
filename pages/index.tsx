1385be0767832f59d5cf1a5ca71023d797b4196f
Shaked Lokits <shaked.lokits@gmail.com>
2022-08-08 23:16:33 +0300

    Add columns rule and extract articles out.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
 import { useLogger } from '../lib/logger';
 import { DataProps } from '../lib/data-fetcher';
 import { Article } from '../components/Article';
+import { FeedEntry } from '../lib/rss-service';
 
 export { getServerSideProps } from '../lib/data-fetcher';
+
+const Articles = ({ entries }: { entries: FeedEntry<any>[] }) => {
+  return (
+    <div className="sm:columns-4xs gap-6 columns-rule">
+      {entries.map((entry, index) => (
+        <Article key={index} entry={entry} />
+      ))}
+    </div>
+  );
+};
 
 const Home = ({ projects, works, articles }: DataProps) => {
   const entries = [...works, ...articles];
   return (
     <div className="container mx-auto max-w-3xl px-9">
-      <div className="sm:columns-4xs gap-6">
-        {entries.map((entry, index) => (
-            <Article key={index} entry={entry} />
-        ))}
-      </div>
+      <Articles entries={entries} />
     </div>
   );
 };
