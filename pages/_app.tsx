4149af5bafca77d321b807940fb67f403517ff2f
Shaked Lokits <shaked.lokits@gmail.com>
2022-08-09 22:35:46 +0300

    Trying to add the links to the fonts in the head to work in safari.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import '../styles/global.css';
-import type {AppProps} from 'next/app';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-const App = ({Component, pageProps}: AppProps) => {
-    return (
-        <>
-            <Head>
-                <title>{process.env.NEXT_PUBLIC_TITLE}</title>
-                <meta name="description" content={process.env.NEXT_PUBLIC_DESCRIPTION}/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>{process.env.NEXT_PUBLIC_TITLE}</title>
+        <meta name="description" content={process.env.NEXT_PUBLIC_DESCRIPTION} />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preload" href="/fonts/Junicode.ttf" as="font" type="font/ttf" crossOrigin="" />
+        <link rel="preload" href="/fonts/Junicode-Bold.ttf" as="font" type="font/ttf" crossOrigin="" />
+        <link rel="preload" href="/fonts/YoungSerif-Regular.otf" as="font" type="font/otf" crossOrigin="" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
