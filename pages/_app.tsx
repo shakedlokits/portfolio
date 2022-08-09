050cac310025b8b82bae490aade439f2a76219ef
Shaked Lokits <shaked.lokits@gmail.com>
2022-08-09 22:50:16 +0300

    Fixed fonts import for better browser support.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,6 @@ const App = ({ Component, pageProps }: AppProps) => {
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
         <meta name="description" content={process.env.NEXT_PUBLIC_DESCRIPTION} />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preload" href="/fonts/Junicode.ttf" as="font" type="font/ttf" crossOrigin="" />
-        <link rel="preload" href="/fonts/Junicode-Bold.ttf" as="font" type="font/ttf" crossOrigin="" />
-        <link rel="preload" href="/fonts/YoungSerif-Regular.otf" as="font" type="font/otf" crossOrigin="" />
       </Head>
       <Component {...pageProps} />
     </>
