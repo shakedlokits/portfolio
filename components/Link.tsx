cdbe3c05d2cd9bf54c5a1292a2c39b9a0bcd6bf4
Shaked Lokits <shaked.lokits@gmail.com>
2022-08-11 11:21:02 +0300

    Opening links in new tabs.

--- components/Link.tsx
+++ components/Link.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react';
 
 export const Link = ({ href, children }: PropsWithChildren<{ href: string }>) => {
   return (
-    <a className="decoration-yellow-200 decoration-double decoration-2 hover:underline" href={href}>
+    <a className="decoration-yellow-200 decoration-double decoration-2 hover:underline" href={href} target="_blank"  rel="noreferrer">
       {children}
     </a>
   );
