683383caf1aded5b6da63d15a72efcf721c8108f
Shaked Lokits <shaked.lokits@gmail.com>
2022-08-24 21:37:12 +0300

    Changed favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,13 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
         <meta name="description" content={process.env.NEXT_PUBLIC_DESCRIPTION} />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Component {...pageProps} />
     </>
