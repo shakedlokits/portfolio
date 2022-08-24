9860f42d5ef9a8c9b7b5cbd36fbd0c4de985fc75
Shaked Lokits <shaked.lokits@gmail.com>
2022-08-24 21:05:21 +0300

    Added static props method.

--- lib/data-fetcher.ts
+++ lib/data-fetcher.ts
@@ -1,24 +1,39 @@
-import {GetServerSidePropsContext} from "next";
-import {FeedEntry, FeedType, getBahanceWorks, getGithubProjects, getMediumArticles} from "./rss-service";
+import { GetServerSidePropsContext } from 'next';
+import { FeedEntry, FeedType, getBahanceWorks, getGithubProjects, getMediumArticles } from './rss-service';
 
-export const getServerSideProps = async ({res}: GetServerSidePropsContext): Promise<{props: DataProps}> => {
-    res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=600');
+export const getServerSideProps = async ({ res }: GetServerSidePropsContext): Promise<{ props: DataProps }> => {
+  res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=600');
 
-    const projects = await getGithubProjects();
-    const works = await getBahanceWorks();
-    const articles = await getMediumArticles();
+  const projects = await getGithubProjects();
+  const works = await getBahanceWorks();
+  const articles = await getMediumArticles();
 
-    return {
-        props: {
-            projects,
-            works,
-            articles,
-        },
-    };
+  return {
+    props: {
+      projects,
+      works,
+      articles,
+    },
+  };
+};
+
+export const getStaticProps = async (): Promise<{ props: DataProps; revalidate: number }> => {
+  const projects = await getGithubProjects();
+  const works = await getBahanceWorks();
+  const articles = await getMediumArticles();
+
+  return {
+    props: {
+      projects,
+      works,
+      articles,
+    },
+    revalidate: 3600,
+  };
 };
 
 export interface DataProps {
-    projects: FeedEntry<FeedType.Github>[];
-    works: FeedEntry<FeedType.Behance>[];
-    articles: FeedEntry<FeedType.Medium>[];
+  projects: FeedEntry<FeedType.Github>[];
+  works: FeedEntry<FeedType.Behance>[];
+  articles: FeedEntry<FeedType.Medium>[];
 }
