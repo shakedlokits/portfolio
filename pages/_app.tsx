98c11eec8a506f64d2ae30ef298395496eac0e24
Shaked Lokits <shaked.lokits@gmail.com>
2022-08-24 23:29:55 +0300

    Reverted back to ico favicon.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
         <meta name="description" content={process.env.NEXT_PUBLIC_DESCRIPTION} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/image/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/image/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
